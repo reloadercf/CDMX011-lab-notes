b112aee64f27ceb7822ea1600619a29a93a08c09
Nohely Badillo <62182593+nohelyx-bc@users.noreply.github.com>
2021-10-26 20:45:33 -0500

    first try of routes

---
+++ daily-journal-app/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
+
+export function App() {
+    return (
+        <BrowserRouter>
+            <div>
+                <h1>
+                    Esta es mi primera App de React uwu!!
+                </h1>
+            </div>
+        </BrowserRouter>
+    )
+}
