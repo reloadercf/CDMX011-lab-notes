4efce6365c3ad3c1fd8a651cddea5e8393018109
Nohely Badillo <62182593+nohelyx-bc@users.noreply.github.com>
2021-11-09 23:33:35 -0600

    added firebase funcions to users auth

--- daily-journal-app/src/lib/secret.js
+++ daily-journal-app/src/lib/secret.js
@@ -1,12 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBVJTi79xXeIA8aDluI0lSe-UHgOlKalwo",
-    authDomain: "daily-jounal.firebaseapp.com",
-    projectId: "daily-jounal",
-    storageBucket: "daily-jounal.appspot.com",
-    messagingSenderId: "809152786591",
-    appId: "1:809152786591:web:4dbe2c15d2409bdc22b2aa"
+  apiKey: "AIzaSyBVJTi79xXeIA8aDluI0lSe-UHgOlKalwo",
+  authDomain: "daily-jounal.firebaseapp.com",
+  projectId: "daily-jounal",
+  storageBucket: "daily-jounal.appspot.com",
+  messagingSenderId: "809152786591",
+  appId: "1:809152786591:web:4dbe2c15d2409bdc22b2aa",
 };
 
-initializeApp(firebaseConfig);
-
-export default firebase;
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
--- daily-journal-app/src/pages/Login.js
+++ daily-journal-app/src/pages/Login.js
@@ -1,6 +1,15 @@
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+
 import "../styles/home.css";
 
 export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const history = useHistory;
+
   return (
     <>
       <form id="login">
